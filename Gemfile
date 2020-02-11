d9e3c042f31ec3dd1eeeceaebbe403572634232a
Jeremy Dumas <mulot90@hotmail.com>
2020-02-11 18:07:31 +0100

    done

--- Gemfile
+++ Gemfile
@@ -52,3 +52,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# FAKER
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+# SIMPLE FORM
+gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles'
+10.times do
+  articles = Article.new(
+    title: Faker::Movies::HarryPotter.character,
+    content: Faker::Movies::HarryPotter.quote
+  )
+  articles.save!
+end
+puts 'Finished!'
